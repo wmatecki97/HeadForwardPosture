f710dcec41dfc62571ba285eb04c74a9c06fd119
Wiktor Matecki <wmatecki97@gmail.com>
2019-07-02 00:01:03 +0200

    initial commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,14 +3,19 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
 import { HomePage } from './home.page';
+import { DeviceMotion } from '@ionic-native/device-motion/ngx';
+import { Vibration } from '@ionic-native/vibration/ngx';
+import { BackgroundMode } from '@ionic-native/background-mode/ngx';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    
     RouterModule.forChild([
       {
         path: '',
@@ -18,6 +23,8 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers:[DeviceMotion, Vibration,BackgroundMode,LocalNotifications
+    ]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,75 @@
-import { Component } from '@angular/core';
+import { DeviceMotion, DeviceMotionAccelerationData } from '@ionic-native/device-motion/ngx';
+import { Component, OnInit } from '@angular/core';
+import { Vibration } from '@ionic-native/vibration/ngx';
+import { BackgroundMode } from '@ionic-native/background-mode/ngx';
+import { interval } from 'rxjs';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  stillBad = false;
+  timeToWait = 2000;
+  x: number;
+  y: number;
+  z: number;
 
+  constructor(
+    private deviceMotion: DeviceMotion,
+    private vibration: Vibration,
+    private backgroundMode: BackgroundMode,
+    private localNotifications: LocalNotifications
+  ) { }
+
+
+  ngOnInit(): void {
+    this.RecognizePosture();
+    this.backgroundMode.enable();
+    interval(this.timeToWait).subscribe(tick => {
+      this.getPosition();
+      if (this.RecognizePosture())
+        this.alarm();
+
+    })
+  }
+
+  getPosition() {
+    // Get the device current acceleration
+    this.deviceMotion.getCurrentAcceleration().then(
+      (acceleration: DeviceMotionAccelerationData) => {
+        this.x = acceleration.x;
+        this.y = acceleration.y;
+        this.z = acceleration.z;
+      }
+    );
+  }
+
+  RecognizePosture() {
+    let isBadPosture = false;
+    if (this.x > 2 && this.x < 9) {
+      isBadPosture = true;
+    }
+    if (this.y > 2 && this.y < 9) {
+      isBadPosture = true;
+    }
+
+    let continues = this.stillBad;
+    this.stillBad = isBadPosture;
+
+    return isBadPosture && continues;
+  }
+
+  alarm() {
+    // Schedule a single notification
+    this.localNotifications.schedule({
+      id: 1,
+      text: 'Single ILocalNotification',
+    })
+      this.vibration.vibrate([100, 100, 100, 100, 100]);
+   
+  }
 }
